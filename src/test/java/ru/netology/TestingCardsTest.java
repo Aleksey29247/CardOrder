2b4e6bde21216ef0945de528f327cd8c065ad4c1
Aleksey <Alik29247@gmail.com>
2022-03-28 21:40:18 +0700

    initial commit

--- src/test/java/ru/netology/TestingCardsTest.java
+++ src/test/java/ru/netology/TestingCardsTest.java
@@ -1,7 +1,53 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestingCardsTest {
 
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void shouldSendForm() {
+        driver.get ("http://localhost:9999");
+        driver.findElement().sendKeys("Шмидт Сергей");
+        driver.findElement().sendKeys("+79991283444");
+        driver.findElement().click();
+        driver.findElement().click();
+        String actualText = driver.findElement().getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
+
+    }
 }
+
