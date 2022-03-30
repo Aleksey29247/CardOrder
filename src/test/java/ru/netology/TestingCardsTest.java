ee04d7cab8ded74d1ab43e6646b09e80f1a4434f
Aleksey <Alik29247@gmail.com>
2022-03-30 20:26:41 +0700

    t

--- src/test/java/ru/netology/TestingCardsTest.java
+++ src/test/java/ru/netology/TestingCardsTest.java
@@ -26,7 +26,7 @@ public class TestingCardsTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
